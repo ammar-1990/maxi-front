197006d417db29972677308fa7ca35521fe79655
ammar <ammar.ali.haidar.1990@gmail.com>
2025-03-30 19:29:27 +0400

    fix(nextConfig):'images'

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
- 
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'files.edgestore.dev',
+        pathname: '/**', // <-- important
       },
-    ]
-
-}
+    ],
+  },
 };
 
 export default nextConfig;
