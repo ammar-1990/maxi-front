729935732dcef2deb4d47337fd1595d7a39098ef
ammar <ammar.ali.haidar.1990@gmail.com>
2025-04-01 19:51:34 +0400

    refine

--- app/_components/LoadingPage.tsx
+++ app/_components/LoadingPage.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 const LoadingPage = (props: Props) => {
   return (
-    <div className='w-full h-[100vh] fixed top-0 left-0 flex items-center justify-center bg-white'>
-        <span className='text-black uppercase font-bold text-6xl animate-pulse select-none'>MAXI</span>
+    <div className='w-full h-[100vh] fixed top-0 left-0 flex items-center justify-center bg-white select-none'>
+        <span className='text-black uppercase font-bold text-6xl animate-pulse select-none pointer-events-none'>MAXI</span>
     </div>
   )
 }
